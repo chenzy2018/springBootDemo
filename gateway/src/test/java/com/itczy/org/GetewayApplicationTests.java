f52d7740d8f8f55ce51531451ee7bac925d82871
chenzy2018 <cz773367227@163.com>
2021-03-01 11:05:44 +0800

    gateway整合v1

---
+++ gateway/src/test/java/com/itczy/org/GetewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itczy.org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
